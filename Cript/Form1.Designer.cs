1bda2681722a89e6585d7454e59d5730fb194051
Matheus Vieira <maath.vieeira@gmail.com>
2018-03-16 19:48:08 -0300

    Trabalho de Criptografia

---
+++ Cript/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Cript
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCript = new System.Windows.Forms.Button();
+            this.btnDescript = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCript
+            // 
+            this.btnCript.Location = new System.Drawing.Point(343, 183);
+            this.btnCript.Name = "btnCript";
+            this.btnCript.Size = new System.Drawing.Size(102, 41);
+            this.btnCript.TabIndex = 0;
+            this.btnCript.Text = "CRIPTOGRAFAR";
+            this.btnCript.UseVisualStyleBackColor = true;
+            this.btnCript.Click += new System.EventHandler(this.btnCript_Click);
+            // 
+            // btnDescript
+            // 
+            this.btnDescript.Location = new System.Drawing.Point(316, 409);
+            this.btnDescript.Name = "btnDescript";
+            this.btnDescript.Size = new System.Drawing.Size(129, 41);
+            this.btnDescript.TabIndex = 1;
+            this.btnDescript.Text = "DESCRIPTOGRAFAR";
+            this.btnDescript.UseVisualStyleBackColor = true;
+            this.btnDescript.Click += new System.EventHandler(this.btnDescript_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox1.Location = new System.Drawing.Point(34, 21);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(411, 156);
+            this.txtBox1.TabIndex = 2;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(34, 243);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(411, 160);
+            this.txtBox2.TabIndex = 3;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(34, 409);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar Tudo";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 472);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnDescript);
+            this.Controls.Add(this.btnCript);
+            this.Name = "Form1";
+            this.Text = "Cript - Cifra de César";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCript;
+        private System.Windows.Forms.Button btnDescript;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Cript/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cript
+{
+    public partial class Form1 : Form
+    {
+
+        String palavra, palavra2, palavra3 = "";
+        String frase;
+        int paa;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCript_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(txtBox1.Text))
+            {
+                MessageBox.Show("Você precisa colocar escrever algo!", "Aviso",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                frase = txtBox1.Text.ToUpper();
+
+                for (int i = 0; i < frase.Length; i++)
+                {
+                    int pa = (int)frase[i];
+                    paa = pa + 10;
+
+                    if (i % 2 == 1)
+                    {
+                        palavra += Char.ConvertFromUtf32(paa).ToLower();
+                    }
+                    else
+                    {
+                        palavra += Char.ConvertFromUtf32(paa).ToUpper();
+                    }
+
+                }
+
+                txtBox2.Text = palavra;
+                txtBox1.Text = "";
+
+                palavra = "";
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = "";
+            txtBox2.Text = "";
+        }
+
+        private void btnDescript_Click(object sender, EventArgs e)
+        {
+
+            if(String.IsNullOrEmpty(txtBox2.Text)){
+                MessageBox.Show("Você precisa colocar escrever algo!", "Aviso",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+            else
+            {
+
+                for (int i = 0; i < txtBox2.Text.Length; i++)
+                {
+                    int pa = (int)txtBox2.Text[i];
+                    int paa = pa - 10;
+
+                    palavra2 += Char.ConvertFromUtf32(paa);
+                }
+
+                txtBox1.Text = palavra2;
+                txtBox2.Text = "";
+
+                palavra2 = "";
+            }
+
+        }
+            
+    }
+}
